55867cf18c7ded7294302d94f2ae86e6ebcf182a
rivky finklshtean <rivsh2005@email.com>
2025-07-22 15:58:36 +0300

    שחזור קובץ IPurchasesRepository.cs

--- legacy/backend/WebApplication1/BLL/IPurchasesRepository.cs
+++ legacy/backend/WebApplication1/BLL/IPurchasesRepository.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using DAL;
+
 namespace BLL
 {
     public interface IPurchasesRepository
